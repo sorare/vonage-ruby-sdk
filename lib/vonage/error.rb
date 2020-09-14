f83a290ae9f0033d6251d3bf6cec1d5ae5369a62
Adrien Montfort <adrien@sorare.com>
2020-09-14 13:13:39 +0200

    Add status code to verify errors

--- lib/vonage/error.rb
+++ lib/vonage/error.rb
@@ -2,5 +2,11 @@
 
 module Vonage
   class Error < StandardError
+    attr_reader :status
+
+    def initialize(message: nil, status: nil)
+      super(message)
+      @status = status&.to_i
+    end
   end
 end
